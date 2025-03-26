c2276bc4f3ab52a27d8bc548fcf54dd3c40e2c83
Pavani <kadaripavani1@gmail.com>
2025-03-26 09:33:28 +0530

    Node Day-1 Fundamentals & FS Module Updated

--- Node/index.js
+++ Node/index.js
@@ -1,22 +1,55 @@
-console.log("Hello Node");
+//importing File System Module
+const fs =require('fs')  
 
-let data = [1,2,3,4,5]
-console.log(data[3])
+//reading a file in a sync way
 
-data.forEach((element, index, arr)=>{
-  console.log(element)
+console.log("-------Reading file in Sync-------------")
+let data =fs.readFileSync('./test.txt','utf-8' );  
+console.log(data);
+console.log("Hello");
+
+//reading a file in async way
+console.log("-------Reading file in Async-------------")
+fs.readFile('./test.txt', 'utf-8', (err, data)=>{
+  console.log(err);
+  console.log(data);
+
+})
+console.log("Test code")
+
+// writing the file in sync way
+console.log("------Writing file in Sync------");
+fs.writeFileSync("./pannu.txt", "Intelligent girl")
+console.log("Writing file successfully..!")
+
+// writing the file in Async way
+console.log("------Writing file in Async------");
+fs.writeFile("./pannu.txt","Topper", (err)=>{
+    console.log(err)
+    console.log("Writing file successfully..!")
+
 })
 
+fs.readFile("./pannu.txt",'utf-8', (data, err)=>{
+  console.log(data)
+  console.log(err)
+} )
 
-let stud =[{
-  name: "Pannu",
-  age :20
-},
-{
-  name:"Pavani",
-  age :20
-}
-]
+// writing file will always overwrite the existing data, to keep the existing data, we use append
 
-console.log(stud)
-console.log(JSON.stringify(stud))
+fs.appendFile('./pannu.txt','\nGood Girl', (err)=>{
+  console.log(err);
+  console.log("File append Successfully")
+})
+
+// delete a file
+// fs.unlinkSync('./pannu.txt')
+
+//OS MODULE
+const os = require('os');
+
+console.log(os.platform());
+console.log(os.hostname());
+console.log(os.freemem());
+console.log(os.homedir());
+
