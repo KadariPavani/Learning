e57dd1a5f1567fe1d1a45c2b733839d4f621813a
Pavani <kadaripavani1@gmail.com>
2025-03-30 22:19:27 +0530

    Done with Day-3 , 2 end points

--- Node/API-Fundamentals/index.js
+++ Node/API-Fundamentals/index.js
@@ -1,49 +1,87 @@
-// Any server that is running locally in our computer that is called as local server and the address is fixed
-// 127.0.0.1 / localhost (domain name)
-// To identify which server like frontend, backend, db we can identify through port number..!
-// port number --> Unique id of the server
-// In node js there is by default inbuild module to create server/ API endpoint by using the functions provided by the module.
-// the module name is http
-
 const http = require('http');
-
-
-
-// create a server
-//127.0.0.1:8000
+const fs = require('fs');
+const url = require('url');
 
 
 http.createServer((req, res) => {
-    // console.log(req);
-    console.log(req.url);
-    // console.log(req.method);
-    
-    // if (req.url == "/add") {
-    //   res.end("Added data")
-    // }
-    // else if(req.url =="/update"){
-    //   res.end("Updated data")
-    // }
+  // console.log(req)
+  // console.log(req.url)
+  // console.log(req.method)
 
-    if(req.url =='/pannu' && req.method=="GET"){
-      res.end("pannu via good");
+
+  //Fetch all the students data --> GET /students
+
+  // if(req.url === "/students" && req.method ==="GET"){
+  //   // res.end("Getting Students data")
+
+  //   fs.readFile("./students.json","utf-8", (err, data)=>{
+  //     if (err === null){
+  //       res.end(data);
+  //     }
+  //     else{
+  //       res.end("Error fetching user data")
+  //     }
+  //   })
+  // }
+
+  //reading the file , it reads everything as a text and its a string
+
+  //ser fetch single student based on id --> GET /students?id=2
+  // How to mention id =2 ??
+  //In node js there is one inbuild module which helps us to extract data from the URL's --> url module
+
+  // console.log(url.parse(req.url,true));
+
+  // res.end("Parse data")
+
+  let parseUrl = url.parse(req.url, true);
+  // console.log(parseUrl);
+  console.log(parseUrl.query.id);
+
+
+  let students = fs.readFileSync("./students.json", "utf-8");
+
+  if (parseUrl.pathname === "/students" && parseUrl.query.id === undefined && req.method === "GET") {
+    res.end(students)
+  }
+  else if (parseUrl.pathname === "/students" && parseUrl.query.id != undefined && req.method === "GET") {
+
+    // res.end("Single student data");
+
+    let studentsArray = JSON.parse(students);
+    let student = studentsArray.find((student) => {
+      return student.id == parseUrl.query.id;
+    })
+    if (student != undefined) {
+      res.end(JSON.stringify(student));
     }
 
-    else if(req.url =='/pannu' && req.method =="POST"){
-      res.end("Pannu via post");
-    }
-    else if(req.url =="/users" && req.method =="POST"){
-      res.end("Created User")
+    else {
+      res.end(JSON.stringify({ message: "Student not found" }));
     }
 
+  }
 
 
-    // res.end("Hello..! This is first response");
+
+    // console.log(student);
+
+    // res.end("Single student data");
 
 
-  })
-  .listen(8000)
 
-//the browser by default seding a get request.
-//To test API (We have to create full fleged frontend api or use API testing tool)
+
+
+
+
+    // res.end("This is our first response");
+
+
+
+
+
+
+
+
+  }).listen(8000)
 
